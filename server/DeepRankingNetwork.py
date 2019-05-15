b7b7c58bbaeabec904f6d4d30b0c635e3798b4f1
LingchangZeng <Zeng774709311@gmail.com>
2019-05-15 14:32:49 +0800

    visual network with feature map

--- server/DeepRankingNetwork.py
+++ server/DeepRankingNetwork.py
@@ -2,7 +2,7 @@ import os,sys,time
 import numpy as np
 from ImgDeal import *
 import tensorflow as tf
-from skimage import exposure
+#from skimage import exposure
 import matplotlib.pyplot as plt
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.training import moving_averages
@@ -44,11 +44,37 @@ def GetCurTime():
     
 def conv2d(x, W):
   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
+  
+def unconv(x,W,Shape):
+    return tf.nn.conv2d_transpose(x, W, Shape, strides=[1,1,1,1], padding='SAME')
+    
 def max_pool_4x4(x):
   return tf.nn.max_pool(x, ksize=[1, 4, 4, 1],
                         strides=[1, 4, 4, 1], padding='SAME')
                         
+def max_pool_with_argmax(net,stride):
+    _, mask = tf.nn.max_pool_with_argmax( net,ksize=[1, stride, stride, 1], strides=[1, stride, stride, 1],padding='SAME')
+    mask = tf.stop_gradient(mask)
+    net = tf.nn.max_pool(net, ksize=[1, stride, stride, 1],strides=[1, stride, stride, 1], padding='SAME') 
+    return net,mask
+
+def un_max_pool(net,mask,stride):
+    ksize = [1, stride, stride, 1]
+    input_shape = net.get_shape().as_list()
+    output_shape = (input_shape[0], input_shape[1] * ksize[1], input_shape[2] * ksize[2], input_shape[3])
+    one_like_mask = tf.ones_like(mask)
+    batch_range = tf.reshape(tf.range(output_shape[0], dtype=tf.int64), shape=[input_shape[0], 1, 1, 1])
+    b = one_like_mask * batch_range
+    y = mask // (output_shape[2] * output_shape[3])
+    x = mask % (output_shape[2] * output_shape[3]) // output_shape[3]
+    feature_range = tf.range(output_shape[3], dtype=tf.int64)
+    f = one_like_mask * feature_range
+    updates_size = tf.size(net)
+    indices = tf.transpose(tf.reshape(tf.stack([b, y, x, f]), [4, updates_size]))
+    values = tf.reshape(net, [updates_size])
+    ret = tf.scatter_nd(indices, values, output_shape)
+    return ret
+    
 def weight_variable( shape, stddev = 0.1):
     initial = tf.truncated_normal(shape, stddev = stddev)
     return initial
@@ -105,14 +131,23 @@ class DRN:
         self.keep_prob = tf.placeholder(tf.float32, name='Keep_Prob')   
         self.is_training = tf.placeholder(tf.bool, name="is_training")
         global_steps = tf.Variable(0, trainable=False)
-        #---------------------------------Design Feature Network----------------------------
+        
+        #--------------------------------- Network Input----------------------------
+        
+        # feature network input
         self.Imgs = tf.placeholder(tf.float32, [None, None], name='Imgs')
         self.Labels = tf.placeholder(tf.float32, [None, 5], name='Labels')
         
+        # re-design network input
         self.backgroud = tf.placeholder(tf.float32, [192*256*3], name='backgroud')
         self.ElementList = tf.placeholder(tf.float32, [None, 192*256*4], name='ElementList')
         self.ElementSize = tf.placeholder(tf.float32, [None, 2], name='ElementSize')
         self.Tags = tf.placeholder(tf.float32,[None, 5], name='Tags')
+        
+        # unpool and unconv network input
+        self.GetFeatureIndex = tf.placeholder(tf.int64, name='Keep_Prob')   
+        #--------------------------------- Re-Design Network ----------------------------
+        '''
         def paste(backgroud, element, Design, Size):
             _element = element[:Size[0],:Size[1]]
             dx = tf.cond(tf.ceil(Design[0]*192) + width < 192, tf.ceil(Design[0]*192) ,lambda: 192-width)
@@ -129,135 +164,122 @@ class DRN:
             return index+1, _backgroud, ElementList, _DesignList, Tag
             
         with tf.variable_scope('redesign_network'):
-            
-            
+        '''
+        
+        #--------------------------------- Design Feature Network ----------------------------
+        
         with tf.variable_scope('design_feature_network'):
             COLLECTIONS = ['COV_NETWORK_VARIABLES', tf.GraphKeys.GLOBAL_VARIABLES]
             
-            # Hidden layer 1 conv : [192 x 256]x3 --> [192 x 256]x64
+            # Hidden layer 1 conv : [256 x 192]x3 --> [256 x 192]x64
             with tf.variable_scope('Cov1') as variable_scope:
-                w_conv = tf.get_variable('w1', regularizer=l2_reg, initializer=weight_variable([3,3,3,64]), collections=COLLECTIONS)
+                w_conv1 = tf.get_variable('w1', regularizer=l2_reg, initializer=weight_variable([3,3,3,64]), collections=COLLECTIONS)
                 Input = tf.reshape(self.Imgs, [-1,256,192,3])
-                temp = conv2d(Input, w_conv)
+                temp = conv2d(Input, w_conv1)
                 temp = batch_normal(temp, self.is_training)
                 op_conv1 = leaky_relu(temp)
                 self.LayerOutput[variable_scope] = tf.transpose(op_conv1, perm=[3,0,1,2])
                 
-                mean, variance = moments(w_conv)
+                mean, variance = moments(w_conv1)
                 tf.summary.scalar("mean", mean)
                 tf.summary.scalar("variance", variance)
                 
-            # Hidden layer 2 conv : [192 x 256]x64 --> [48 x 64]x64
+            # Hidden layer 2 conv : [256 x 192]x64 --> [64 x 48]x64
             with tf.variable_scope('Cov2') as variable_scope:
-                w_conv = tf.get_variable('w2', regularizer=l2_reg, initializer=weight_variable([3,3,64,64]), collections=COLLECTIONS)
+                w_conv2 = tf.get_variable('w2', regularizer=l2_reg, initializer=weight_variable([3,3,64,64]), collections=COLLECTIONS)
                 
-                temp = conv2d(op_conv1, w_conv)
+                temp = conv2d(op_conv1, w_conv2)
                 temp = batch_normal(temp, self.is_training)
                 op_conv2 = leaky_relu(temp)
-                op_pool2 = max_pool_4x4(op_conv2)
+                op_pool2, pool_mask2 = max_pool_with_argmax(op_conv2, 4)
                 
                 self.LayerOutput[variable_scope] = tf.transpose(op_pool2, perm=[3,0,1,2])
-                mean, variance = moments(w_conv)
+                mean, variance = moments(w_conv2)
                 tf.summary.scalar("mean", mean)
                 tf.summary.scalar("variance", variance)
                 
-            # Hidden layer 3 conv : [48 x 64]x64 --> [12 x 16]x64
+            # Hidden layer 3 conv : [64 x 48]x64 --> [16 x 12]x64
             with tf.variable_scope('Cov3') as variable_scope:
-                w_conv = tf.get_variable('w3', regularizer=l2_reg, initializer=weight_variable([3,3,64,64]), collections=COLLECTIONS)
+                w_conv3 = tf.get_variable('w3', regularizer=l2_reg, initializer=weight_variable([3,3,64,64]), collections=COLLECTIONS)
                 
-                temp = conv2d(op_pool2, w_conv)
+                temp = conv2d(op_pool2, w_conv3)
                 temp = batch_normal(temp, self.is_training)
                 op_conv3 = leaky_relu(temp)
-                op_pool3 = max_pool_4x4(op_conv3)
+                op_pool3, pool_mask3 = max_pool_with_argmax(op_conv3, 4)
                 
                 #self.LayerOutput[variable_scope] = tf.transpose(op_pool3, perm=[3,0,1,2])
-                mean, variance = moments(w_conv)
+                mean, variance = moments(w_conv3)
                 tf.summary.scalar("mean", mean)
                 tf.summary.scalar("variance", variance)
                 
-            # Hidden layer 4 conv : [12 x 16]x64 --> [3 x 4]x64
+            # Hidden layer 4 conv : [16 x 12]x64 --> [4 x 3]x64
             with tf.variable_scope('Cov4') as variable_scope:
-                w_conv = tf.get_variable('w4', regularizer=l2_reg, initializer=weight_variable([3,3,64,64]), collections=COLLECTIONS)
+                w_conv4 = tf.get_variable('w4', regularizer=l2_reg, initializer=weight_variable([3,3,64,64]), collections=COLLECTIONS)
                 
-                temp = leaky_relu(conv2d(op_pool3, w_conv))
+                temp = leaky_relu(conv2d(op_pool3, w_conv4))
                 temp = batch_normal(temp, self.is_training)
                 op_conv4 = leaky_relu(temp)
-                op_pool4= max_pool_4x4(op_conv4)
+                op_pool4, pool_mask4= max_pool_with_argmax(op_conv4, 4)
 
                 #self.LayerOutput[variable_scope] = tf.transpose(op_pool4, perm=[3,0,1,2])
-                mean, variance = moments(w_conv)
+                mean, variance = moments(w_conv4)
                 tf.summary.scalar("mean", mean)
                 tf.summary.scalar("variance", variance)
                 
             # Fully connected layer 2_2: [3 x 4]x64 --> Vector[256]
             with tf.variable_scope('fc1') as variable_scope: 
-                w_fc2_2 = tf.get_variable('w5', regularizer=l2_reg, initializer=weight_variable([3*4*64, 256]), collections=COLLECTIONS)
-                b_fc2_2 = tf.get_variable('b5', regularizer=l2_reg, initializer=bias_variable([256], 0.5), collections=COLLECTIONS)
+                w_fc1 = tf.get_variable('w5', regularizer=l2_reg, initializer=weight_variable([3*4*64, 256]), collections=COLLECTIONS)
+                b_fc1 = tf.get_variable('b5', regularizer=l2_reg, initializer=bias_variable([256], 0.5), collections=COLLECTIONS)
                 
                 h_pool4_flat = tf.reshape(op_pool4, [-1,3*4*64])
-                h_fc2_2 = tf.nn.dropout(leaky_relu(tf.matmul(h_pool4_flat, w_fc2_2) + b_fc2_2), self.keep_prob)
-
-
-        '''
-        #---------------------------------Semantic embedding network----------------------------
-        self.Tag = tf.placeholder(tf.float32, [16], name='Tag')
-        Tags = control_flow_ops.cond(
-            tf.equal(tf.shape(self.NegativeImgs)[1],0), lambda:  tf.tile(tf.reshape(self.Tag, [1,16]), [PosBatchSize, 1]),
-            lambda: tf.tile(tf.reshape(self.Tag, [1,16]), [tf.add(PosBatchSize,NegBatchSize), 1]))
-            
-        with tf.variable_scope('semantic_embedding_network'):
-            COLLECTIONS = ['SEM_EMBEDDING_NETWORK', tf.GraphKeys.GLOBAL_VARIABLES]
-            
-            # Fully connected layer 1 : Vector[16] --> Vector[64]
-            with tf.variable_scope('fc1') as variable_scope: 
-                Input = tf.reshape(Tags, [-1,16])
-                
-                w_fc1 = tf.get_variable('w1', regularizer=l2_reg, initializer=weight_variable([16, 64], 0.5), collections=COLLECTIONS)
-                b_fc1 = tf.get_variable('b1', regularizer=l2_reg, initializer=bias_variable([64], 0.5), collections=COLLECTIONS)
-                
-                temp = tf.matmul(Input, w_fc1) + b_fc1
-                h_fc1 = leaky_relu(temp)
-                
-                self.LayerOutput[variable_scope] = tf.reshape(h_fc1,[-1])
-                
-            # Fully connected layer 2_1 : Vector[16] --> Vector[64]
-            with tf.variable_scope('fc2_1') as variable_scope: 
-                w_fc2_1 = tf.get_variable('w2', regularizer=l2_reg, initializer=weight_variable([64, 64], 0.5), collections=COLLECTIONS)
-                b_fc2_1 = tf.get_variable('b2', regularizer=l2_reg, initializer=bias_variable([64], 0.5), collections=COLLECTIONS)
-                
-                temp = tf.matmul(h_fc1, w_fc2_1) + b_fc2_1
-                h_fc2_1 = leaky_relu(temp)
-                
-                self.LayerOutput[variable_scope] = tf.reshape(h_fc2_1,[-1])
-                
-        #---------------------------------Semantic scoring network----------------------------
+                h_fc1 = tf.nn.dropout(leaky_relu(tf.matmul(h_pool4_flat, w_fc1) + b_fc1), self.keep_prob)
         
-        # concat (Vector[64] Vector[256]) --> Vector[320] 
-        input_fc3 = tf.concat([h_fc2_1,h_fc2_2], axis = 1)
-        '''
+        with tf.variable_scope('unconv_unpool_network'):
+            LayerFeature = tf.reshape(op_pool4[self.GetFeatureIndex,:,:,:,], [1,4,3,64])
+            with tf.variable_scope('unconv3') as variable_scope:
+                unpool4 = un_max_pool(LayerFeature,  tf.reshape(pool_mask4[self.GetFeatureIndex,:,:,:,], [1,4,3,64]), 4)
+                temp = leaky_relu(unpool4)
+                print(temp.get_shape())
+                unconv4 = unconv(temp, w_conv4, [1, 16, 12, 64])
+                
+            with tf.variable_scope('unconv2') as variable_scope:
+                unpool2 = un_max_pool(unconv4, tf.reshape(pool_mask3[self.GetFeatureIndex,:,:,:,], [1,16,12,64]), 4)
+                temp = leaky_relu(unpool2)
+                unconv3 = unconv(temp, w_conv3, [1, 64, 48, 64])
+                
+            with tf.variable_scope('unconv1') as variable_scope:
+                unpool3 = un_max_pool(unconv3, tf.reshape(pool_mask2[self.GetFeatureIndex,:,:,:,], [1,64,48,64]), 4)
+                temp = leaky_relu(unpool3)
+                unconv2 = unconv(temp, w_conv2, [1, 256, 192, 64])  
+                
+            with tf.variable_scope('output') as variable_scope:
+                temp = leaky_relu(unconv2)
+                self.FeatureMap = unconv(temp, w_conv1, [1, 256, 192, 3])  
+            
+            
         with tf.variable_scope('semantic_scoring_network'):
             COLLECTIONS = ['SEM_SCORING_NETWORK', tf.GraphKeys.GLOBAL_VARIABLES]
             
             #Fully Connected layer 3 : Vector[320] --> Vector[256]
             with tf.variable_scope('fc2') as variable_scope: 
-                w_fc3 = tf.get_variable('w3', regularizer=l2_reg, initializer=weight_variable([256, 256]), collections=COLLECTIONS)
-                b_fc3 = tf.get_variable('b3', regularizer=l2_reg, initializer=bias_variable([256]), collections=COLLECTIONS)
-                h_fc3 = tf.nn.dropout(leaky_relu(tf.matmul(h_fc2_2, w_fc3) + b_fc3), self.keep_prob)
+                w_fc2 = tf.get_variable('w3', regularizer=l2_reg, initializer=weight_variable([256, 256]), collections=COLLECTIONS)
+                b_fc2 = tf.get_variable('b3', regularizer=l2_reg, initializer=bias_variable([256]), collections=COLLECTIONS)
+                h_fc2 = tf.nn.dropout(leaky_relu(tf.matmul(h_fc1, w_fc2) + b_fc2), self.keep_prob)
                 
 
                 
             #Fully Connected layer 4 : Vector[256] --> Vector[128]
             with tf.variable_scope('fc3') as variable_scope: 
-                w_fc4 = tf.get_variable('w4', regularizer=l2_reg, initializer=weight_variable([256, 128]), collections=COLLECTIONS)
-                b_fc4 = tf.get_variable('b4', regularizer=l2_reg, initializer=bias_variable([128]), collections=COLLECTIONS)
-                h_fc4 = tf.nn.dropout(leaky_relu(tf.matmul(h_fc3, w_fc4) + b_fc4), self.keep_prob)
+                w_fc3 = tf.get_variable('w4', regularizer=l2_reg, initializer=weight_variable([256, 128]), collections=COLLECTIONS)
+                b_fc3 = tf.get_variable('b4', regularizer=l2_reg, initializer=bias_variable([128]), collections=COLLECTIONS)
+                h_fc3 = tf.nn.dropout(leaky_relu(tf.matmul(h_fc2, w_fc3) + b_fc3), self.keep_prob)
                 
 
                 
         #----------------------------------------    Output    -----------------------------------  
         with tf.variable_scope('Output'):
             w_op = tf.get_variable('w_op', regularizer=l2_reg, initializer=weight_variable([128, 5]), collections=COLLECTIONS)
-            self.Score = tf.nn.softmax(tf.matmul(h_fc4, w_op))
+            self.Score = tf.nn.softmax(tf.matmul(h_fc3, w_op))
 
             
         #----------------------------------------    Loss    -----------------------------------    
@@ -303,8 +325,18 @@ class DRN:
                 if index%16 == 0 and index != 0:
                     plt.show()
             print(LayerName.name, output.shape)
+            
+    def GetFeatureMap(self, Imgs, Index = 0):
+        Imgs = Imgs.reshape([-1,256*192*3])
+        FeatureMap = self.sess.run(self.FeatureMap, feed_dict = {
+                                                    self.Imgs: Imgs,
+                                                    self.keep_prob: 1.0,
+                                                    self.is_training: False,
+                                                    self.GetFeatureIndex: Index
+                                                    })
+        return FeatureMap
         
-    def GetScore(self, img, is_training = False, use_shuffle = False):
+    def GetScore(self, img, is_training = False):
         Imgs = img.reshape([-1,256*192*3])
         return np.array(self.sess.run(self.Score, feed_dict={ 
                                                         self.Imgs: Imgs,
@@ -312,7 +344,7 @@ class DRN:
                                                         self.is_training: is_training
                                                        }))
     
-    def Train(self, Times, SaveTimes = 10, keep_prob = 0.5, is_training = True, use_shuffle = True):
+    def Train(self, Times, SaveTimes = 10, keep_prob = 0.5, is_training = True):
         LearnStepCounter = 0
         DataSet = LoadingTrainingData()
         Writer = tf.summary.FileWriter(os.path.join(sys.path[0], 'logs/', GetCurTime() + r'/'), self.sess.graph)
@@ -354,7 +386,7 @@ class DRN:
         plt.show()
         return DesignList
         
-    def TestLoss(self, is_training = False, use_shuffle = False):
+    def TestLoss(self, is_training = False):
         DataSet = LoadingTestingData()
         Loss = self.sess.run(self.loss,
                    feed_dict={ 
